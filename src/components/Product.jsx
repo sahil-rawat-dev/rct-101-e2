4ab6898b88aef8aae06d78884152ff22622cc61b
sahil-rawat-dev <sahilrawat2710@gmail.com>
2022-05-30 17:05:57 +0530

    only product page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-
-const Product = () => {
+import {Text,Image,Box,Stack,Heading,Tag,TagLabel,Flex,
+} from "@chakra-ui/react";
+const Product = ({ imageSrc, title, category, price, gender }) => {
   // TODO: Remove below const and instead import them from chakra
-  const Text = () => <div />;
-  const Image = () => <div />;
-  const Box = () => <div />;
-  const Stack = () => <div />;
-  const Heading = () => <div />;
-  const Tag = () => <div />;
-  const TagLabel = () => <div />;
+  // const Text = () => <div />;
+  // const Image = () => <div />;
+  // const Box = () => <div />;
+  // const Stack = () => <div />;
+  // const Heading = () => <div />;
+  // const Tag = () => <div />;
+  // const TagLabel = () => <div />;
   return (
-    <Stack data-cy="product">
+    <Stack data-cy="product" src={imageSrc}>
       <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
+      <Text data-cy="product-category">{category}</Text>
       <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
+        <TagLabel data-cy="product-gender">{gender}</TagLabel>
       </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
+      <Heading data-cy="product-title">{title}</Heading>
+      <Box data-cy="product-price">{"Rs."}{price}</Box>
     </Stack>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React,{ useEffect, useState } from "react";
+import { Flex, Grid } from "@chakra-ui/react";
+import axios from "axios";
+
+import Product from "./Product";
 
 const Products = () => {
-  // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  
+  const [pageFilter, setPageFilter] = useState({
+    pageNumber: 1,
+    limit: 3,
+    totalCount: 0,
+  });
+  const [products, setProducts] = useState([]);
+
+  const { pageNumber, limit } = pageFilter;
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await axios.get(
+        `http://localhost:8080/products?_page=${pageNumber}&_limit=${limit}`
+      );
+      // setPageFilter({
+      //   ...pageFilter,
+      //   totalCount: Number(response.headers["x-total-count"]),
+      // });
+
+      setProducts(response.data);
+    };
+    getProducts();
+    
+  });
 
   return (
     <Flex>
       {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+      <Grid w="full" gridGap={4} gridTemplateColumns="1fr 1fr 1fr">
+        {products.map((p) => (
+          <Product key={p.id} {...p} />
+        ))}
+      </Grid>
       {/* Pagination */}
     </Flex>
   );
